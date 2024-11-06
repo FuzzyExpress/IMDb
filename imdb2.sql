22774c2100497ac2a50b76231ba3243e37424458
Fuzzy Express <127012288+FuzzyExpress@users.noreply.github.com>
2024-11-06 10:39:22 -0600

    First
    1

---
+++ imdb2.sql
@@ -0,0 +1,161 @@
+CLEAR SCR
+
+DROP TABLE movie                	CASCADE CONSTRAINTS;
+DROP TABLE tv_show                	CASCADE CONSTRAINTS;
+DROP TABLE tv_episode               CASCADE CONSTRAINTS;
+
+DROP TABLE director             	CASCADE CONSTRAINTS;
+DROP TABLE director_directs_mov    	CASCADE CONSTRAINTS;
+DROP TABLE director_directs_tv     	CASCADE CONSTRAINTS;
+
+DROP TABLE actor             		CASCADE CONSTRAINTS;
+DROP TABLE actor_acts_for_mov     	CASCADE CONSTRAINTS;
+DROP TABLE actor_acts_for_tv     	CASCADE CONSTRAINTS;
+
+DROP TABLE writer             		CASCADE CONSTRAINTS;
+DROP TABLE writer_writes_mov     	CASCADE CONSTRAINTS;
+DROP TABLE writer_writes_tv     	CASCADE CONSTRAINTS;
+
+
+
+CREATE TABLE movie
+(
+    MTitle      	VARCHAR2    (32)    CONSTRAINT MTitle    NOT NULL,
+    MDescription    VARCHAR2    (1024),
+    MStudio   	 	VARCHAR2    (32),
+    MState          VARCHAR2    (16),
+    MDetails        VARCHAR2    (64),
+    MRating         NUMBER, --     (2, 2),
+    MTagline        VARCHAR2    (32),
+    CONSTRAINT movie_pkey PRIMARY KEY (Title)
+);
+
+CREATE TABLE tv_show
+(
+    TVTitle     	VARCHAR2    (32)    CONSTRAINT Title    NOT NULL,
+    TVDescription	VARCHAR2    (1024),
+    TVStudio   	    VARCHAR2    (32),
+    TVState  		VARCHAR2    (16),
+    TVDetails		VARCHAR2    (64),
+    TVRating 		NUMBER, --      (2, 2),
+    TVTagline		VARCHAR2    (32),
+    CONSTRAINT movie_pkey PRIMARY KEY (Title)
+);
+
+CREATE TABLE tv_episode
+(
+    EP_Title      	VARCHAR2    (32)    CONSTRAINT Title    NOT NULL,
+    EP_Season     	VARCHAR2    (1024),
+	EP_Duration   	VARCHAR2    (32),
+    EP_Rating       NUMBER, --      (2, 2),
+    ShowTitle       VARCHAR2    (32),
+    CONSTRAINT movie_pkey PRIMARY KEY (Title)
+);
+
+
+
+CREATE TABLE director
+(
+    DName           VARCHAR2    (32)    CONSTRAINT DName    NOT NULL,
+    DBithDay        DATE,
+    DBithLoc        VARCHAR2    (64),
+    DPhoto          VARCHAR2    (512), -- cdn link
+    CONSTRAINT director_pkey PRIMARY KEY (DName)
+);
+
+CREATE TABLE director_directs_mov
+(
+    MovieTitle      VARCHAR2    (32)
+    MovieDirName    VARCHAR2    (32)
+);
+
+CREATE TABLE director_directs_tv
+(
+    ShowTitle       VARCHAR2    (32)
+    ShowDirName     VARCHAR2    (32)
+);
+
+
+
+CREATE TABLE actor
+(
+    AName           VARCHAR2    (32)    CONSTRAINT AName    NOT NULL,
+    ABithDay        DATE,
+    ABithLoc        VARCHAR2    (64),
+    APhoto          VARCHAR2    (512), -- cdn link
+    CONSTRAINT actor_pkey PRIMARY KEY (AName)
+);
+
+CREATE TABLE actor_acts_for_mov
+(
+    MovieTitle      VARCHAR2    (32)
+    MovieActName    VARCHAR2    (32)
+);
+
+CREATE TABLE actor_acts_for_tv
+(
+    ShowTitle       VARCHAR2    (32)
+    ShowActName     VARCHAR2    (32)
+);
+
+
+
+CREATE TABLE writer
+(
+    WName           VARCHAR2    (32)    CONSTRAINT WName    NOT NULL,
+    WBithDay        DATE,
+    WBithLoc        VARCHAR2    (64),
+    WPhoto          VARCHAR2    (512), -- cdn link
+    CONSTRAINT director_pkey PRIMARY KEY (WName)
+);
+
+CREATE TABLE writer_writes_mov
+(
+    MovieTitle      VARCHAR2    (32)
+    MovieWrtName    VARCHAR2    (32)
+);
+
+CREATE TABLE writer_writes_tv
+(
+    ShowTitle       VARCHAR2    (32)
+    ShowWrtName     VARCHAR2    (32)
+);
+
+
+
+
+CREATE TABLE Awards
+(
+	AwrName			VARCHAR2 	(16) 	CONSTRAINT AwrName    NOT NULL,
+    AwrYear         DATE    			CONSTRAINT AwrYear    NOT NULL,
+	AwrType			VARCHAR2    (16),
+	AwrGenre		VARCHAR2    (16),
+	Awr_TV_Award	VARCHAR2    (32),
+	Awr_Mov_Award	VARCHAR2    (32),
+
+    
+    CONSTRAINT award_name PRIMARY KEY (AwrName)
+    -- CONSTRAINT award_year PRIMARY KEY (AwrYear)
+);
+
+CREATE TABLE writer_writes_mov
+(
+    AwrName      VARCHAR2    (16)
+    AwrGiver     VARCHAR2    (32)
+);
+
+
+
+ALTER TABLE director_directs
+ADD CONSTRAINT movie_fk FOREIGN KEY(MovieTitle)
+REFERENCES movie(Title);
+
+ALTER TABLE director_directs
+ADD CONSTRAINT director_fk FOREIGN KEY(DirName)
+REFERENCES director(DName);
+
+
+DESCRIBE movie;
+DESCRIBE director;
+
+
